d2bd053e18db0b99b6a149ddfc0acca14ce82b26
Cuong Nguyen <dr.yami.moe@gmail.com>
2022-11-10 05:33:17 +0200

    Move WIP

--- src/modules/game/game.view.tsx
+++ src/modules/game/game.view.tsx
@@ -1,7 +1,8 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import clsx from 'clsx'
 import XMarkIcon from '@heroicons/react/24/solid/XMarkIcon'
 import ShieldCheckIcon from '@heroicons/react/24/solid/ShieldCheckIcon'
+import { js as Easystar } from 'easystarjs'
 
 import {
 	BLOCK_TILE,
@@ -37,10 +38,39 @@ const tilesToDict = (tiles: Point[]) => {
 
 export const GameView = () => {
 	const [heroPos] = useState<Point>(() => [...START_POINT])
+	const [movePos, setMovePos] = useState<Point>()
+	const [easystar] = useState(() => new Easystar())
 	const movableTiles = useMemo(
 		() => tilesToDict(getMovableTiles(heroPos, { mobility: 5, grid: GRIDS })),
 		[heroPos],
 	)
+	const [moveTiles, setMoveTiles] = useState<Point[]>([])
+
+	useEffect(() => {
+		easystar.setGrid(GRIDS)
+		easystar.setAcceptableTiles([0, FOREST_TILE])
+		easystar.setTileCost(FOREST_TILE, FOREST_TILE)
+		easystar.enableSync()
+	}, [easystar])
+
+	useEffect(() => {
+		if (movePos) {
+			easystar.findPath(heroPos[0], heroPos[1], movePos[0], movePos[1], (path) => {
+				setMoveTiles(
+					path
+						.filter((v) => v.x !== heroPos[0] || v.y !== heroPos[1])
+						.map((v) => [v.x, v.y]),
+				)
+			})
+			easystar.calculate()
+		}
+	}, [movePos, heroPos, easystar])
+
+	const prepareMoveHandler = (x: number, y: number) => () => {
+		if (movableTiles[getTileKey([x, y])]) {
+			setMovePos([x, y])
+		}
+	}
 
 	return (
 		<div className="flex h-[720px] w-[1280px] rounded bg-slate-700 p-4 text-white">
@@ -55,10 +85,18 @@ export const GameView = () => {
 									'group relative aspect-square basis-0 rounded border',
 									tile !== BLOCK_TILE && 'hover:ring hover:ring-red-400',
 								)}
+								onClick={prepareMoveHandler(j, i)}
 							>
-								{isStartPoint(j, i) && <img src={HERO_ICON} />}
-								{tile === BLOCK_TILE && <XMarkIcon />}
-								{tile === FOREST_TILE && <ShieldCheckIcon />}
+								<div className="absolute inset-0">
+									{isStartPoint(j, i) && <img src={HERO_ICON} />}
+								</div>
+								<div className="absolute inset-0">
+									{tile === BLOCK_TILE && <XMarkIcon />}
+								</div>
+								<div className="absolute inset-0">
+									{tile === FOREST_TILE && <ShieldCheckIcon />}
+								</div>
+
 								<div className="absolute inset-0">
 									{j}-{i}
 								</div>
@@ -70,6 +108,8 @@ export const GameView = () => {
 											'bg-green-400/50',
 										tile === BLOCK_TILE && 'bg-red-900/50',
 										tile !== BLOCK_TILE && 'group-hover:bg-rose-400/30',
+										moveTiles.find((p) => p[0] === j && p[1] === i) &&
+											'bg-amber-500/50',
 									)}
 								/>
 							</div>
